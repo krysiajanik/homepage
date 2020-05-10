64ba83b1b7224d178a967791a936829eeaf31b80
krysiajanik <a.krystyna.janik@gmail.com>
2020-05-10 20:02:21 +0200

    new notes for week four

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-console.log('Hello there')
-console.log('General Kenobi!')
+console.log("Hello there");
+console.log("General Kenobi!");
+console.log("Witaj strudzony przybyszu. Internet to miejsce dziwne, ale wspaniałe. A najlepszym, co może Cię spotkać w Internecie, są memy. Jeśli podzielasz to zdanie, zapraszam na podstronę Memes.");
